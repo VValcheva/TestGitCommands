616d87a9b214ee94adfe1937df27262a9c809df6
VValcheva <venetsiya.jorova@abv.bg>
2022-05-19 13:11:29 +0300

    Added four commands

--- src/main/java/com/example/demo/commands/Git.java
+++ src/main/java/com/example/demo/commands/Git.java
@@ -2,5 +2,9 @@ package com.example.demo.commands;
 
 public class Git {
 
-    private String gitCommand;
+    private String git_clone;
+    private String git_add;
+    private String git_commit;
+    private String git_push;
+
 }
