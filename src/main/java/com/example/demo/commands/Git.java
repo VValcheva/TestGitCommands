1bfefa47ddea1604e4592ae2f729e9281c1ebbe8
VValcheva <venetsiya.jorova@abv.bg>
2022-05-19 13:20:39 +0300

    Maked changes

--- src/main/java/com/example/demo/commands/Git.java
+++ src/main/java/com/example/demo/commands/Git.java
@@ -3,7 +3,9 @@ package com.example.demo.commands;
 public class Git {
 
     private String gitCommand;
+    private String gitPush;
     private String gitPull;
     private String gitMerge;
+
 
 }
